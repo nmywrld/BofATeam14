0e3955d5ff24d023a37755c98d95f05ed51ef04f
wui-hong <khoowuihong@yahoo.com.sg>
2024-05-13 11:59:05 +0800

    Temp commit

--- continuousTradingEngine.py
+++ continuousTradingEngine.py
@@ -8,12 +8,48 @@ sell_market_heap = []
 
 
 #assume heaps have already been updated
-with open('samplecsv.csv') as file_obj: 
-    #ignore header row
-    heading = next(file_obj) 
-    reader_obj = csv.reader(file_obj) 
+
+def csv_to_ArrayDict(filename):
+    with open(filename, mode='r') as infile:
+        reader = csv.DictReader(infile)
+        data = [row for row in reader]
+    return data
+
+def csv_to_dict(filename):
+    with open(filename, mode='r') as infile:
+        reader = csv.DictReader(infile)
+        data = {row[next(iter(reader.fieldnames))]: {key: value for key, value in row.items() if key != next(iter(reader.fieldnames))} for row in reader}
+    return data
+order_data = csv_to_ArrayDict('./test-set/input_orders.csv')
+print(order_data)
+
+def time_string_to_int(time_string):
+    #input "9:02:43" output: time
+    split_arr = time_string.split(":")
+    final_time = 0
+    final_time += int(split_arr[0]) * 60 * 60
+    final_time += int(split_arr[1]) * 60
+    final_time += int(split_arr[2])
+    return final_time
+
+for order in order_data:
+    if order['Side'] == "Sell":
+        currObj = orderNode(order['Time'], order['OrderID'], order['Instrument'], order['Quantity'], order['Client'], order['Price'], order['Side'], client_data[order['Client']]["Rating"], client_data[order['Client']]["PositionCheck"])
+        if order['Price'] == "Market":
+            sell_market_heap.append(currObj)
+        else:
+            sell_heap.append(currObj)
+    else:
+        currObj = orderNode(order['Time'], order['OrderID'], order['Instrument'], order['Quantity'], order['Client'], order['Price'], order['Side'], client_data[order['Client']]["Rating"], client_data[order['Client']]["PositionCheck"])
+        
+        if order['Price'] == "Market":
+            buy_market_heap.append(currObj)
+        else:
+            buy_heap.append(currObj)
+
+
     for row in reader_obj: 
-        time = time
+        time = row[0]
         order_id = order_id
         instrument = instrument
         quantity = quantity
